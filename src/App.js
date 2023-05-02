c78517861fa02d77720d094bce55060d76628564
alexpuentes <alejandro@puentestoledano.com>
2023-05-02 19:18:01 +0200

    navbar de test - maquetación

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navegationbar';
+import NumberSender from './components/numberSender';
+import Productos from './components/Productos';
+import Carrito from './components/Carrito';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<NumberSender />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/carrito" element={<Carrito />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
