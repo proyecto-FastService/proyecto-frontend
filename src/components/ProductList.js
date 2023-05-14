8f2544d2c83b4900c8d307730b2b3818ced86de6
alexpuentes <alejandro@puentestoledano.com>
2023-05-14 17:08:23 +0200

    api funciona

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,39 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import ProductCard from './ProductCard';
 import { CartContext } from '../context/cartContext';
-import patatasFritas from './patatas-fritas.jpg';
-import hamburguesa from './hamburguesa.jpg';
 
 function ProductList(props) {
-  const { onAddToCart } = React.useContext(CartContext);
+  const { onAddToCart } = useContext(CartContext);
+  const [productos, setProductos] = useState([]);
 
-  const products = [
-    {
-      id: 1,
-      title: 'Patatas fritas',
-      description: 'Una deliciosa hamburguesa con queso cheddar y tocino',
-      price: '$10.99',
-      image: patatasFritas
-    },
-    {
-      id: 2,
-      title: 'Hamburguesa',
-      description: 'Una pizza de pepperoni y champiñones recién horneada',
-      price: '$12.99',
-      image: hamburguesa
-    },
-    // más productos aquí
-  ];
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://127.0.0.1:8000/api/prueba2/1/');
+      const data = await response.json();
+      setProductos(data.productosEnStock);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="product-list">
-      {products.map(product => (
+      {productos.map((product) => (
         <ProductCard
           key={product.id}
-          title={product.title}
-          description={product.description}
-          price={product.price}
-          image={product.image}
+          title={product.nombre}
+          description={product.descripcion}
+          price={`$${product.precio}`}
+          image={product.imagen}
           onAddToCart={() => onAddToCart(product)}
         />
       ))}
@@ -42,3 +32,4 @@ function ProductList(props) {
 }
 
 export default ProductList;
+
