87a6c3b9b90f9787b4b672f70a3312b1dcc48f47
alexpuentes <alejandro@puentestoledano.com>
2023-05-14 18:12:47 +0200

    ekisde

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,12 +5,25 @@ import { CartContext } from '../context/cartContext';
 function ProductList(props) {
   const { onAddToCart } = useContext(CartContext);
   const [productos, setProductos] = useState([]);
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('http://127.0.0.1:8000/api/prueba2/1/');
+      const token = localStorage.getItem('token');
+      let response;
+      if (token != null) {
+        console.log(token)
+        let ruta = "http://127.0.0.1:8000/api/prueba2/1/" + token;
+        response = await fetch(ruta);
+      } else {
+        response = await fetch('http://127.0.0.1:8000/api/prueba2/1/');
+      }
       const data = await response.json();
-      setProductos(data.productosEnStock);
+      setProductos(data);
+      console.log(productos)
+      if (token == null) {
+        localStorage.setItem('token', data.token);
+      }
     }
     fetchData();
   }, []);
@@ -32,4 +45,5 @@ function ProductList(props) {
 }
 
 export default ProductList;
+
 
