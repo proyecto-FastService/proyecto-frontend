44eac9ff4c29d874e56ab1112d45e4e6884e45af
alexpuentes <alejandro@puentestoledano.com>
2023-05-21 16:29:02 +0200

    cambios admin

--- src/components/administrador/PanelAdmin.js
+++ src/components/administrador/PanelAdmin.js
@@ -1,30 +1,51 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Admin() {
+  const [mesas, setMesas] = useState([]);
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  
+
+  useEffect(() => {
+    const obtenerMesas = async () => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/admObtenerTodasMesas/${token}`);
+        setMesas(response.data);
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+      } catch (error) {
+        console.error('Error al obtener las mesas:', error);
+      }
+    };
+
+    obtenerMesas();
+  }, []);
+
+  const handleClickMesa = (mesaId) => {
+    navigate(`/productos/0/${mesaId}`);
+  };
+
   return (
     <div>
       <h1>Panel de Administrador</h1>
 
-      <div className="card-container">
-        <Link to="/productos/0/mesa1" className="card">
-          <h2>Mesa 1</h2>
-          <p>Detalles</p>
-        </Link>
-        <Link to="/productos/0/mesa2" className="card">
-          <h2>Mesa 2</h2>
-          <p>Detalles</p>
-        </Link>
-        <Link to="/productos/0/mesa3" className="card">
-          <h2>Mesa 3</h2>
-          <p>Detalles</p>
-        </Link>
+      <div>
+        {mesas.map((mesa) => (
+          <div key={mesa.id} className="card" onClick={() => handleClickMesa(mesa.id)}>
+            <h2>Mesa {mesa.numero}</h2>
+            <p>Descripción de la mesa</p>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
 export default Admin;
+
+
 
 
 
