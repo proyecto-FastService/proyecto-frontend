275b05426db8c8eb39cf22932c02c334d89c146e
alexpuentes <alejandro@puentestoledano.com>
2023-05-24 19:01:44 +0200

    aja

--- src/components/administrador/ProductEditor.js
+++ src/components/administrador/ProductEditor.js
@@ -4,67 +4,70 @@ import { useNavigate } from 'react-router-dom';
 import { Modal, Button, Form } from 'react-bootstrap';
 
 function ProductEditor() {
-    const navigate = useNavigate();
-    const token = localStorage.getItem('token');
-    const [productos, setProductos] = useState([]);
-    const [showModal, setShowModal] = useState(false);
-    const [selectedProduct, setSelectedProduct] = useState(null);
-    const [editedProduct, setEditedProduct] = useState({
-        nombre: '',
-        existencias: 0,
-        precio: 0,
-        descripcion: '',
-        ingredientes: '',
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  const [productos, setProductos] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [editedProduct, setEditedProduct] = useState({
+    id: '',
+    nombre: '',
+    existencias: 0,
+    precio: 0,
+    descripcion: '',
+    ingredientes: '',
+  });
+
+  useEffect(() => {
+    const obtenerListadoProducto = async () => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/admObtenerListadoProducto/${token}`);
+        const data = response.data.productos;
+        setProductos(data);
+      } catch (error) {
+        console.error('Error al obtener el listado de productos:', error);
+      }
+    };
+
+    obtenerListadoProducto();
+  }, [token]);
+
+  const handleEditProduct = (product) => {
+    setSelectedProduct(product);
+    setEditedProduct({
+      id: product.id,
+      nombre: product.nombre,
+      existencias: product.existencias,
+      precio: product.precio,
+      descripcion: product.descripcion,
+      ingredientes: product.ingredientes,
     });
+    setShowModal(true);
+  };
 
-    useEffect(() => {
-        const obtenerListadoProducto = async () => {
-            try {
-                const response = await axios.get(`http://127.0.0.1:8000/api/admObtenerListadoProducto/${token}`);
-                const data = response.data.productos;
-                setProductos(data);
-            } catch (error) {
-                console.error('Error al obtener el listado de productos:', error);
-            }
-        };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
-        obtenerListadoProducto();
-    }, [token]);
+  const handleInputChange = (e) => {
+    setEditedProduct({
+      ...editedProduct,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleEditProduct = (product) => {
-        setSelectedProduct(product);
-        setEditedProduct({
-            nombre: product.nombre,
-            existencias: product.existencias,
-            precio: product.precio,
-            descripcion: product.descripcion,
-            ingredientes: product.ingredientes,
-        });
-        setShowModal(true);
-    };
-
-    const handleCloseModal = () => {
-        setShowModal(false);
-    };
-
-    const handleInputChange = (e) => {
-        setEditedProduct({
-            ...editedProduct,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleSaveChanges = async () => {
-        try {
-            await axios.put(`http://127.0.0.1:8000/api/admEditarProducto/${token}/${selectedProduct.id}`, editedProduct);
-            // Realizar acciones adicionales si es necesario
-            setShowModal(false);
-        } catch (error) {
-            console.error('Error al guardar los cambios del producto:', error);
-        }
-    };
-
-    // ...
+  const handleSaveChanges = async () => {
+    try {
+      await axios.put(
+        `http://127.0.0.1:8000/api/admEditarProducto/${token}/${selectedProduct.id}`,
+        editedProduct
+      );
+      // Realizar acciones adicionales si es necesario
+      setShowModal(false);
+    } catch (error) {
+      console.error('Error al guardar los cambios del producto:', error);
+    }
+  };
 
     return (
         <div>
