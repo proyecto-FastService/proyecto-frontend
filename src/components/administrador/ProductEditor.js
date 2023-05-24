84462e40174e627926ecd46ba7d43d273bdc22f7
alexpuentes <alejandro@puentestoledano.com>
2023-05-24 19:11:05 +0200

    precios XD

--- src/components/administrador/ProductEditor.js
+++ src/components/administrador/ProductEditor.js
@@ -4,70 +4,72 @@ import { useNavigate } from 'react-router-dom';
 import { Modal, Button, Form } from 'react-bootstrap';
 
 function ProductEditor() {
-  const navigate = useNavigate();
-  const token = localStorage.getItem('token');
-  const [productos, setProductos] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState(null);
-  const [editedProduct, setEditedProduct] = useState({
-    id: '',
-    nombre: '',
-    existencias: 0,
-    precio: 0,
-    descripcion: '',
-    ingredientes: '',
-  });
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+    const [productos, setProductos] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [editedProduct, setEditedProduct] = useState({
+        id: '',
+        nombre: '',
+        existencias: 0,
+        precio: 0,
+        descripcion: '',
+        ingredientes: '',
+        alergenos: '',
+    });
 
-  useEffect(() => {
-    const obtenerListadoProducto = async () => {
-      try {
-        const response = await axios.get(`http://127.0.0.1:8000/api/admObtenerListadoProducto/${token}`);
-        const data = response.data.productos;
-        setProductos(data);
-      } catch (error) {
-        console.error('Error al obtener el listado de productos:', error);
-      }
+    useEffect(() => {
+        const obtenerListadoProducto = async () => {
+            try {
+                const response = await axios.get(`http://127.0.0.1:8000/api/admObtenerListadoProducto/${token}`);
+                const data = response.data.productos;
+                setProductos(data);
+            } catch (error) {
+                console.error('Error al obtener el listado de productos:', error);
+            }
+        };
+
+        obtenerListadoProducto();
+    }, [token]);
+
+    const handleEditProduct = (product) => {
+        setSelectedProduct(product);
+        setEditedProduct({
+            id: product.id,
+            nombre: product.nombre,
+            existencias: product.existencias,
+            precio: product.precio,
+            descripcion: product.descripcion,
+            ingredientes: product.ingredientes,
+            alergenos: product.alergenos
+        });
+        setShowModal(true);
     };
 
-    obtenerListadoProducto();
-  }, [token]);
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
-  const handleEditProduct = (product) => {
-    setSelectedProduct(product);
-    setEditedProduct({
-      id: product.id,
-      nombre: product.nombre,
-      existencias: product.existencias,
-      precio: product.precio,
-      descripcion: product.descripcion,
-      ingredientes: product.ingredientes,
-    });
-    setShowModal(true);
-  };
+    const handleInputChange = (e) => {
+        setEditedProduct({
+            ...editedProduct,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  const handleInputChange = (e) => {
-    setEditedProduct({
-      ...editedProduct,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSaveChanges = async () => {
-    try {
-      await axios.put(
-        `http://127.0.0.1:8000/api/admEditarProducto/${token}/${selectedProduct.id}`,
-        editedProduct
-      );
-      // Realizar acciones adicionales si es necesario
-      setShowModal(false);
-    } catch (error) {
-      console.error('Error al guardar los cambios del producto:', error);
-    }
-  };
+    const handleSaveChanges = async () => {
+        try {
+            await axios.post(
+                `http://127.0.0.1:8000/api/admEditarProducto/${token}/${selectedProduct.id}`,
+                editedProduct
+            );
+            // Realizar acciones adicionales si es necesario
+            setShowModal(false);
+        } catch (error) {
+            console.error('Error al guardar los cambios del producto:', error);
+        }
+    };
 
     return (
         <div>
@@ -80,6 +82,7 @@ function ProductEditor() {
                         <p>Precio: {producto.precio}</p>
                         <p>Descripción: {producto.descripcion}</p>
                         <p>Ingredientes: {producto.ingredientes}</p>
+                        <p>Añergenos: {producto.alergenos}</p>
                         <button onClick={() => handleEditProduct(producto)}>Editar Producto</button>
                     </div>
                 ))}
@@ -101,7 +104,7 @@ function ProductEditor() {
                         </Form.Group>
                         <Form.Group controlId="formPrecio">
                             <Form.Label>Precio</Form.Label>
-                            <Form.Control type="number" name="precio" value={editedProduct.precio} onChange={handleInputChange} />
+                            <Form.Control type="number" name="precios" value={editedProduct.precio} onChange={handleInputChange} />
                         </Form.Group>
                         <Form.Group controlId="formDescripcion">
                             <Form.Label>Descripción</Form.Label>
@@ -110,6 +113,10 @@ function ProductEditor() {
                         <Form.Group controlId="formIngredientes">
                             <Form.Label>Ingredientes</Form.Label>
                             <Form.Control as="textarea" name="ingredientes" value={editedProduct.ingredientes} onChange={handleInputChange} />
+                        </Form.Group>
+                        <Form.Group controlId="formPrecio">
+                            <Form.Label>Alergenos</Form.Label>
+                            <Form.Control type="number" name="alergenos" value={editedProduct.alergenos} onChange={handleInputChange} />
                         </Form.Group>
                     </Form>
                 </Modal.Body>
