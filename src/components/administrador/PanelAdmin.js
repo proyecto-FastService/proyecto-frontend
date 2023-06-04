03a5c3e054cb7c5585f1ba3ed9f604ffe896505a
alexpuentes <alejandro@puentestoledano.com>
2023-06-04 18:29:13 +0200

    fixeos demo

--- src/components/administrador/PanelAdmin.js
+++ src/components/administrador/PanelAdmin.js
@@ -5,17 +5,20 @@ import { FaUser } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 import { useParams } from 'react-router-dom';
 
-
 function Admin() {
   const [mesas, setMesas] = useState([]);
   const [refresh, setRefresh] = useState(false); // Estado para refrescar el componente
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
   const { numeroMesa } = useParams();
+  const [loaded, setLoaded] = useState(false);
+  const [loadedNumeroMesa, setLoadedNumeroMesa] = useState(false);
+  const [reloadPage, setReloadPage] = useState(false); // Estado para controlar la recarga de la página
 
   useEffect(() => {
     if (numeroMesa) {
       localStorage.setItem('mesa', numeroMesa);
+      setLoadedNumeroMesa(true); // Indicar que se ha cargado el número de mesa correctamente
     }
   }, [numeroMesa]);
 
@@ -41,29 +44,46 @@ function Admin() {
         const response = await axios.get(`https://daw206.medacarena.es/public/api/admObtenerTodasMesas/${token}`);
         setMesas(response.data.mesas);
         console.log(mesas);
+
+        setLoaded(true); // Indicar que se ha cargado correctamente
       } catch (error) {
         console.error('Error al obtener las mesas:', error);
       }
     };
 
-    obtenerMesas();
-  }, [refresh]); // Escucha cambios en el estado 'refresh'
+    if (loadedNumeroMesa) {
+      obtenerMesas();
+    }
+  }, [refresh, loadedNumeroMesa]); // Escucha cambios en los estados 'refresh' y 'loadedNumeroMesa'
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setRefresh((prevRefresh) => !prevRefresh);
-      console.log("Me recargo");
-    }, 5000); // Recarga los componentes cada segundo
+    if (loaded && !reloadPage) {
+      // Guardar token y mesa en el localStorage
+      localStorage.setItem('token', token);
+      localStorage.setItem('mesa', numeroMesa);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [refresh]); // Escucha cambios en el estado 'refresh'
+      // Realizar la recarga de la página
+      setReloadPage(true);
+      // window.location.reload();
+    }
+  }, [loaded, reloadPage, token, numeroMesa]);
+
+  useEffect(() => {
+    if (reloadPage) {
+      const interval = setInterval(() => {
+        setRefresh((prevRefresh) => !prevRefresh);
+        console.log('Me recargo');
+      }, 40000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [reloadPage]);
 
   const handleClickMesa = (mesaId) => {
     navigate(`/productos/admin/0/${mesaId}`);
   };
-
 
   const confirmarAtenderMesa = async (mesa) => {
     const result = await Swal.fire({
@@ -92,6 +112,64 @@ function Admin() {
       }
     }
   };
+
+  const handleCerrarSesion = async () => {
+    const result = await Swal.fire({
+      title: 'Cerrar sesión',
+      text: '¿Estás seguro de que quieres cerrar sesión?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar',
+    });
+
+    if (result.isConfirmed) {
+      try {
+        const response = await axios.get(`https://daw206.medacarena.es/public/api/cerrarSesion/${token}`);
+        console.log(response.data);
+        // Limpiar el token del localStorage
+        localStorage.removeItem('token');
+
+        Swal.fire('Sesión cerrada', 'La sesión ha sido cerrada correctamente.', 'success');
+
+        navigate('/'); // Redireccionar a la página de inicio de sesión
+      } catch (error) {
+        console.error('Error al cerrar sesión:', error);
+        Swal.fire('Error', 'Ocurrió un error al cerrar la sesión.', 'error');
+      }
+    }
+  };
+
+  const handleLimpiarMesas = async () => {
+    const result = await Swal.fire({
+      title: 'Limpiar mesas',
+      text: '¿Estás seguro de que quieres limpiar las mesas?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar',
+    });
+
+    if (result.isConfirmed) {
+      try {
+        const response = await axios.get(`https://daw206.medacarena.es/public/api/limpiarMesas/${token}`);
+        console.log(response.data);
+
+        Swal.fire('Mesas limpiadas', 'Las mesas han sido limpiadas correctamente.', 'success');
+
+        setRefresh(!refresh); // Actualizar el estado 'refresh' para refrescar el componente
+      } catch (error) {
+        console.error('Error al limpiar las mesas:', error);
+        Swal.fire('Error', 'Ocurrió un error al limpiar las mesas.', 'error');
+      }
+    }
+  };
+
+
 
   return (
     <div className='d-flex' style={{ flexDirection: 'column' }}>
@@ -127,12 +205,20 @@ function Admin() {
               <button className='btn-admin mt-3' onClick={() => handleClickMesa(mesa.id)}>
                 Administrar
               </button>
+
             </div>
           </div>
         ))}
       </div>
+      <button className='btn-admin mt-3' onClick={handleCerrarSesion}>
+        Cerrar sesión
+      </button>
+      <button className='btn-admin mt-3' onClick={handleLimpiarMesas}>
+        Limpiar Mesas
+      </button>
     </div>
   );
 }
 
 export default Admin;
+
